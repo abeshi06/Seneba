fc9b9d477fef46844f022673a1da7cc68cb6786f
jhsAbe <abeshi06@hotmail.co.jp>
2016-07-07 15:27:39 +0900

    メニューの追加と検索機能の追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,6 +51,12 @@ class TasksController < ApplicationController
     end
   end
 
+  def search
+    @search_name = params['search']['name']
+    @tasks = Task.where("name like '%#{@search_name}%'")
+    render :index
+  end
+
   # DELETE /tasks/1
   # DELETE /tasks/1.json
   def destroy
